c841937a776bef0a12cdc41dc15033614a6926e8
nforgetgrace <nforgetgrace@gmail.com>
2023-05-23 17:46:50 +0900

    프로젝트 초기화

---
+++ mobility/src/main/java/com/rookie/mobility/api/controller/BasicController.java
@@ -0,0 +1,35 @@
+package com.rookie.mobility.api.controller;
+
+import org.springframework.context.annotation.Description;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+* @package : com.ktds.rookie.library.domain.controller
+* @name : BasicController.java
+* @date : 2023-04-05 오후 13:45
+* @author : Jung Jae gook
+* @version : 1.0.0
+* @modifyed :
+* @description : 샘플 컨트롤러
+**/
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class BasicController {
+
+	
+    @GetMapping("/get")
+    @Description("테스트 get 함수")
+    public String get(@RequestParam String name){
+
+        return name;
+    }
+}
---
+++ mobility/src/main/resources/h2db/data.sql
@@ -0,0 +1,62 @@
+INSERT INTO tb_member (
+    pk_member
+    ) 
+values (
+    1
+    );
+
+
+INSERT INTO tb_device (
+    pk_device   
+    ) 
+values (
+    1
+    );
+
+
+
+INSERT INTO tb_using (
+    pk_using
+    ) 
+values (
+    1    
+    );
+
+
+INSERT INTO tb_payment (
+    pk_payment
+    ) 
+values (
+    1    
+    );
+
+
+
+INSERT INTO tb_basic_price (
+    pk_basic_price
+    ) 
+values (
+    1    
+    );
+
+
+
+INSERT INTO tb_coopon (
+    pk_coopon
+    ) 
+values (
+    1    
+    );
+
+
+
+INSERT INTO tb_coopon_member (
+    pk_coopon_member
+    ) 
+values (
+    1    
+    );
+
+
+
+
