460df831be28e8676d16c8f34021474534e42670
jowonjun <wonjun0824@gmail.com>
2023-05-24 11:03:26 +0900

    setup

---
+++ mobility/src/main/java/com/rookie/mobility/api/device/controller/DeviceController.java
@@ -0,0 +1,47 @@
+package com.rookie.mobility.api.device.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+* @package : com.ktds.rookie.library.domain.controller
+* @name : BasicController.java
+* @date : 2023-04-05 오후 13:45
+* @author : Jung Jae gook
+* @version : 1.0.0
+* @modifyed :
+* @description : 샘플 컨트롤러
+**/ 
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api") 
+@Slf4j
+public class DeviceController {
+
+	
+    @PostMapping("/device")
+    public String memberJoin(@RequestBody String name,
+    						@RequestBody String id,
+    						@RequestBody String pw,
+    						@RequestBody String phone,
+    						@RequestBody String gender,
+    						@RequestBody String nick){
+
+    	String rst = name + " " + id + " " + pw + " " + phone + " " + gender + " " + nick;
+        return rst;
+    }
+    
+    @GetMapping("/device2")
+    public String m(@RequestParam String name){
+
+        return name;
+    }
+}
--- mobility/src/main/java/com/rookie/mobility/api/join/controller/JoinController.java
+++ mobility/src/main/java/com/rookie/mobility/api/join/controller/JoinController.java
@@ -24,7 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/api") 
 @Slf4j
-public class BasicController {
+public class JoinController {
 
 	
     @PostMapping("/join")
